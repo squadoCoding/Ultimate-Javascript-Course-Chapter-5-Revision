7c592d433bcf51cbb2160623df696c300179eca0
squadoCoding <106444206+squadoCoding@users.noreply.github.com>
2022-11-07 04:08:38 +0000

    completed chapter rivision

--- index.js
+++ index.js
@@ -32,8 +32,8 @@
 // console.log(nums);
 // console.log(nums.sort());// actual array will be affected
 // const compare = (a, b) => {
-  // return a - b;// arrange numbers in accending oredr
-  // return b - a;// arrange numbers in descending oredr
+// return a - b;// arrange numbers in accending oredr
+// return b - a;// arrange numbers in descending oredr
 // }
 // console.log(nums.sort(compare));
 
@@ -43,5 +43,54 @@
 // console.log(nums.reverse());// actual array will be changed
 
 // Looping through arrays
-// Javascript provides many additional loops to loop through arrays or objects.
-// I will continue revision tommorw
+// Javascript provides many additional loops to loop through arrays or objects.We can loop through arrays using the classicle for loop or while loop and do-while loops in javascript but we have some great alternatives to minimize the effort to loop through arrays.
+
+const myNums = [2, 35, 78, 10, 43];
+
+// forEach loops: forEach() loop performs the given operations for each element in the array.
+// myNums.forEach((num, index, array) => {
+//   num += 2;
+//   console.log(num);
+//   console.log(index);
+//   console.log(array);
+// });
+
+// map: This method also executes the given operations on all the elements of the array but the differene is that this method creates a new array created with all the elements we returne frem the callback function().
+// In the following example we are creating an array which contains the double of each element of "myNums"
+// const newNumsArr = myNums.map((num, index, array) => {
+//   return num * 2;
+// });
+
+// console.log(newNumsArr);
+
+// filter method is used to check a particular condition for each element in the array, if the condition is false then that element will be removed and the elements for which the condition is true that elements will be added to an array and that array will be returned (note that origional array will not be affected);
+// const lessThan40 = myNums.filter((num, index, arr) => {
+//   return num <= 40;
+// });
+// console.log(lessThan40);
+
+// reduce: This method is used to reduce an array to form a single value.
+// const addition = myNums.reduce((previousNum, currentNumber, currentIndex, arr) => {
+//   return previousNum + currentNumber;
+// });
+
+// console.log(addition);
+
+// There is an Array.from() method in javascript which is used to convert objects in arrays
+// console.log(Array.from("Siddhant"));
+// // This wil only work on iterable objects
+// console.log(Array.from({
+//   name: "Siddhant",
+//   class: 8,
+//   favFood: "Good foor good life"
+// }));
+
+// for of loop is used to loop through arrays, as we know that arrays are also objects so we can eacily loop through arrays using for of loops as arrays are iterable
+for(let num of myNums){
+  console.log(num);
+}
+
+// for in loop can be used to get kees{indexes} from an array
+for(let index in myNums){
+  console.log(index);
+}
